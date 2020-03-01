9445b7f79b6f2340b33083fa5ed6b81061459e1c
SolutionfulOracle <54331292+SolutionfulOracle@users.noreply.github.com>
2020-03-01 13:23:46 -0700

    Update Move.cs

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -7,36 +7,40 @@ public class Move : MonoBehaviour
 
     private float speed;
     public Vector2 originalDirection;
+    private Vector2 directionVector;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        speed = 1.5;
+        speed = 1.5f;
+        float direction = transform.rotation.z;
 
+        switch (direction)
+        {
+            case 0:
+                directionVector = Vector2.left;
+                break;
+
+            case 90:
+                directionVector = Vector2.down;
+                break;
+
+            case 180:
+                directionVector = Vector2.right;
+                break;
+
+            case 270:
+                directionVector = Vector2.up;
+                break;
+
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        float direction = transform.rotation;
-        switch (direction) {
-            case 0:
-                transform.position = (Vector2)transform.position + Vector2.left * speed * Time.deltaTime;
-                break;
-
-            case 90:
-                transform.position = (Vector2)transform.position + Vector2.down * speed * Time.deltaTime;
-                break;
-
-            case 180:
-                transform.position = (Vector2)transform.position + Vector2.right * speed * Time.deltaTime;
-                break;
-
-            case 270:
-                transform.position = (Vector2)transform.position + Vector2.up * speed * Time.deltaTime;
-                break;
-
-        }
+        transform.position = (Vector2)transform.position + directionVector * speed * Time.deltaTime;
 
     }
 }
