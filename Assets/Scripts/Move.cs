eebb465373a5dfb0b80eee215f7f6b4add5f153b
SolutionfulOracle <54331292+SolutionfulOracle@users.noreply.github.com>
2020-03-01 13:11:07 -0700

    direction correction

--- Assets/Scripts/Move.cs
+++ Assets/Scripts/Move.cs
@@ -11,14 +11,32 @@ public class Move : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        speed = 1;
+        speed = 1.5;
 
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = (Vector2)transform.position + Vector2.left * speed * Time.deltaTime;
+        float direction = transform.rotation;
+        switch (direction) {
+            case 0:
+                transform.position = (Vector2)transform.position + Vector2.left * speed * Time.deltaTime;
+                break;
+
+            case 90:
+                transform.position = (Vector2)transform.position + Vector2.down * speed * Time.deltaTime;
+                break;
+
+            case 180:
+                transform.position = (Vector2)transform.position + Vector2.right * speed * Time.deltaTime;
+                break;
+
+            case 270:
+                transform.position = (Vector2)transform.position + Vector2.up * speed * Time.deltaTime;
+                break;
+
+        }
 
     }
 }
