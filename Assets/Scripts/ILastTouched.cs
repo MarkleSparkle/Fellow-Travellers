af41421638b31218358d9e2f6433f3aa73eac56d
MarkleSparkle <markfrezelljr@yahoo.ca>
2020-03-20 21:33:21 -0600

    Commented on ILastTouched

--- Assets/Scripts/ILastTouched.cs
+++ Assets/Scripts/ILastTouched.cs
@@ -23,13 +23,21 @@ public class ILastTouched : MonoBehaviour
     void OnTriggerEnter(Collider col)
     {
         iLastEntered = col;
-
+        //if the Collider the car just entered with is a BoxCollider, that means that it has gotten to close to the car behind it,
+             //then decrease the speed of the car, as long as it is not still in collision with the car.
     }
 
     void OnTriggerExit(Collider col)
     {
         iLastExited = col;
-
+        //if the Collider that you just exited was a BoxCollider (a car),
+            //get the velocity of the object that you entered (because that would be the car in front of you)
+            //that way, the current car can match the speed of the car in front, as long as it is not too close to the car
+            //(meaning that the car's CapsuleCollider has collided with the car in front again).
 
     }
+
+    //used https://www.youtube.com/watch?v=7GASazoHlck
+    //and https://answers.unity.com/questions/736962/distinguish-between-multiple-box-colliders.html
+    //and 
 }
