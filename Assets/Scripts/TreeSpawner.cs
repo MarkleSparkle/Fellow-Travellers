b568d872498809a7a3838874a369c926d3978fa1
MarkleSparkle <markfrezelljr@yahoo.ca>
2020-10-02 15:23:01 -0600

    Began implementing tree spawning
    Added a Tree Spawner and began implementing it

---
+++ Assets/Scripts/TreeSpawner.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TreeSpawner : MonoBehaviour
+{//TreeSpawner spawns trees in the corners of the map at the beginning of the game
+
+    public GameObject treeFab; //see what I did there? ;D
+
+    void Start()
+    {
+        Camera camera = Camera.main;
+        float halfHeight = camera.orthographicSize;
+        float halfWidth = camera.aspect * halfHeight;
+        Vector3 position;
+
+        /* Spawning the Top Left*/
+
+        //defining boundary coordinates
+        float horizontalMin = -halfWidth - 1.2f;
+        float horizontalMax = -halfWidth - 1.2f;
+        float verticalMin = halfHeight - 1.2f;
+        float verticalMax = halfHeight + 1.2f;
+
+
+        for(int i = 0; i < 100; i++)
+        {
+            position = new Vector3(Random.Range(horizontalMin, horizontalMax),Random.Range(verticalMin,verticalMax),-1);
+            Instantiate(treeFab, position, Quaternion.identity);
+        }
+    }
+}
