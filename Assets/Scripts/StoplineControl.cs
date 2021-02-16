014168236c7370493bf008fbf992f4fcdc3fe43d
SolutionfulOracle <54331292+SolutionfulOracle@users.noreply.github.com>
2021-02-15 23:25:07 -0700

    fixed stop lines
    stop lines now inherit from the scene instead of spawning in on play

--- Assets/Scripts/StoplineControl.cs
+++ Assets/Scripts/StoplineControl.cs
@@ -3,7 +3,12 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StoplineControl : MonoBehaviour
-{//This script will be used to instantiate stoplines and allow the user to toggle their rigid bodies.  Stoplight animations to be addded later...
+{//This script will be used to manage light and traffic direction toggle
+    public GameObject leftLine;
+    public GameObject rightLine;
+    public GameObject topLine;
+    public GameObject bottomLine;
+
     public GameObject stopLine;
     public BoxCollider2D leftBox;
     public BoxCollider2D rightBox;
@@ -29,33 +34,33 @@ public class StoplineControl : MonoBehaviour
     public string DirectionOfTraffic;
 
     // Start is called before the first frame update
-    void Start()//start will create four instantiations of the stop line prefab. right and left will have enabled colliders by default
+    void Start()//right and left will have enabled colliders by default
     {
-        GameObject leftLine = Instantiate(stopLine);//Create left stopline
+        /*GameObject leftLine = Instantiate(stopLine);//Create left stopline
         leftLine.transform.Rotate(0, 0, 90);
         leftLine.transform.position = new Vector3(-2.4f, -1, 0.1f);//approximated.  lines also need to be between the background and the cars on the Z axis
-        leftLine.transform.localScale = new Vector2(2, 2);//original prefab will be upscaled by a factor of 2. you made it too small, mark
+        leftLine.transform.localScale = new Vector2(2, 2);*/
         leftBox = leftLine.AddComponent<BoxCollider2D>() as BoxCollider2D;
         leftBox.isTrigger = true;
 
-        GameObject rightLine = Instantiate(stopLine);//right side stopline
+        /*GameObject rightLine = Instantiate(stopLine);//right side stopline
         rightLine.transform.Rotate(0, 0, 90);
         rightLine.transform.position = new Vector3(2.2f, 1, 0.1f);
-        rightLine.transform.localScale = new Vector2(2, 2);
+        rightLine.transform.localScale = new Vector2(2, 2);*/
         rightBox = rightLine.AddComponent<BoxCollider2D>() as BoxCollider2D;
         rightBox.isTrigger = true;
 
 
-        GameObject topLine = Instantiate(stopLine);//top line doesn't need rotation
+        /*GameObject topLine = Instantiate(stopLine);//top line doesn't need rotation
         topLine.transform.position = new Vector3(-1.1f, 2.2f, 0.1f);
-        topLine.transform.localScale = new Vector2(2, 2);
+        topLine.transform.localScale = new Vector2(2, 2);*/
         topBox = topLine.AddComponent<BoxCollider2D>() as BoxCollider2D;
         topBox.isTrigger = true;
         topBox.enabled = false;
 
-        GameObject bottomLine = Instantiate(stopLine);//hahaha I'm totally losing it
+       /* GameObject bottomLine = Instantiate(stopLine);//hahaha I'm totally losing it
         bottomLine.transform.position = new Vector3(0.9f, -2.2f, 0.1f);
-        bottomLine.transform.localScale = new Vector2(2, 2);
+        bottomLine.transform.localScale = new Vector2(2, 2);*/
         bottomBox = bottomLine.AddComponent<BoxCollider2D>() as BoxCollider2D;
         bottomBox.isTrigger = true;
         bottomBox.enabled = false;
